54fc4892792410ba88ad2b141ea4ce9249cb4150
derInformatiker <68816018+derInformatiker@users.noreply.github.com>
2021-05-08 14:10:22 +0200

    Add files via upload

---
+++ challenge1/dataset.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+import cv2
+
+class ImgDataset(Dataset):
+    def __init__(self,df,mode,transforms = None):
+        self.imageID = df['ImageID']
+        self.labels = df['label']
+        self.transforms = transforms
+        self.labelmap = {'redbull':0,'mercedes':1}
+        self.mode = mode
+        
+    def __getitem__(self,x):
+        path = self.imageID.iloc[x]
+        label = float(self.labelmap[self.labels.iloc[x]])
+        i = cv2.imread(f'data/{self.mode}/'+str(path)+'.jpg')
+        i = cv2.cvtColor(i, cv2.COLOR_BGR2RGB)
+        
+        if self.transforms:
+            i = self.transforms(image = i)['image']
+            
+        i = torch.tensor(i) / 255.0
+        #.unsqueeze_(-1)
+        i = i.permute(2,0,1)
+        if self.mode != 'test':
+            return i, label
+        else:
+            return i
+    
+    def __len__(self):
+        return len(self.imageID)
+    
+def getTrainDs(train_tr = None):
+    train_df = pd.read_csv('data/train.csv')
+    return ImgDataset(train_df,'train',train_tr)
+
+def getValDs(val_tr):
+    val_df = pd.read_csv('data/val.csv')
+    return ImgDataset(val_df,'val',val_tr)
+
+def getTestDs(test_tr):
+    val_df = pd.read_csv('data/sample_submission.csv')
+    return ImgDataset(val_df,'test',test_tr)
+
+def writeSub(p):
+    test_df = pd.read_csv('data/sample_submission.csv')
+    
---
+++ challenge1/model.py
@@ -0,0 +1,107 @@
+import os
+import dataset
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from efficientnet_pytorch import EfficientNet
+
+
+class Classifier(pl.LightningModule):
+
+    def __init__(self,args):
+        super().__init__()
+        self.args = args
+        
+        #self.resnet = torch.hub.load('pytorch/vision:v0.9.0', 'resnet50', pretrained=False,num_classes = 2)
+        self.resnet = EfficientNet.from_pretrained('efficientnet-b3',num_classes = 2)
+
+    def forward(self, x):
+        # in lightning, forward defines the prediction/inference actions
+        x = self.resnet(x)
+        prob = F.softmax(x)
+        return x, prob
+    def training_step(self, batch, batch_idx):
+        # training_step defined the train loop.
+        # It is independent of forward
+        x, y = batch
+        p, _ = self(x)
+        loss = F.cross_entropy(p, y.long())
+        # Logging to TensorBoard by default
+        self.log('train_loss', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        # training_step defined the train loop.
+        # It is independent of forward
+        x, y = batch
+        p, _ = self(x)
+        
+        loss = F.cross_entropy(p, y.long())
+        f1 = f1_loss(y,p.argmax(1))
+        # Logging to TensorBoard by default
+        self.log('val_loss', loss)
+        self.log('val_f1',f1)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        # training_step defined the train loop.
+        # It is independent of forward
+        
+        x, y = batch
+        p, _ = self(x)
+        
+        loss = F.cross_entropy(p, y.long())
+        f1 = f1_loss(y,p.argmax(1))
+        # Logging to TensorBoard by default
+        self.log('test_loss', loss)
+        self.log('test_f1',f1)
+        return loss
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=3e-4)
+        return optimizer
+    
+    def train_dataloader(self):
+        train_ds = dataset.getTrainDs(self.args['train_tr'])
+        loader= DataLoader(train_ds,batch_size = self.args['batch_size'],num_workers = 6,shuffle=True)
+        return loader
+    
+    def val_dataloader(self):
+        val_ds = dataset.getValDs(self.args['val_tr'])
+        loader= DataLoader(val_ds,batch_size = self.args['batch_size'],num_workers = 6)
+        return loader
+    def test_dataloader(self):
+        val_ds = dataset.getValDs(self.args['val_tr'])
+        loader= DataLoader(val_ds,batch_size = self.args['batch_size'],num_workers = 6)
+        return loader
+    
+    def predict_dataloader(self):
+        val_ds = dataset.getTestDs(self.args['val_tr'])
+        loader= DataLoader(val_ds,batch_size = self.args['batch_size'],num_workers = 6)
+        return loader
+    
+    
+def f1_loss(y_true:torch.Tensor, y_pred:torch.Tensor, is_training=False) -> torch.Tensor:
+
+    assert y_true.ndim == 1
+    assert y_pred.ndim == 1 or y_pred.ndim == 2
+
+    if y_pred.ndim == 2:
+        y_pred = y_pred.argmax(dim=1)
+
+
+    tp = (y_true * y_pred).sum().to(torch.float32)
+    tn = ((1 - y_true) * (1 - y_pred)).sum().to(torch.float32)
+    fp = ((1 - y_true) * y_pred).sum().to(torch.float32)
+    fn = (y_true * (1 - y_pred)).sum().to(torch.float32)
+
+    epsilon = 1e-7
+
+    precision = tp / (tp + fp + epsilon)
+    recall = tp / (tp + fn + epsilon)
+
+    f1 = 2* (precision*recall) / (precision + recall + epsilon)
+    f1.requires_grad = is_training
+    return f1
